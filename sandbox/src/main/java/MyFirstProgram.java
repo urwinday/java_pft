b31f946c54ebe1a86e1f62adb7f3c64d187790fa
urwinday <tregiven@gmail.com>
2023-02-08 21:19:49 +0400

    test commit git

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -3,5 +3,6 @@ public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, TEST!");
 		System.out.println("Test GIT!");
+		System.out.println("Test commit GIT!");
 	}
 }
